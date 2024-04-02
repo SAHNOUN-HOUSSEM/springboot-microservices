25eb5b54458879df1fc16b59cc4768e97e7fbd6a
SahnounHoussem <sahnoun.houssem05@gmail.com>
2024-04-02 22:01:36 +0100

    creating frauds modules and connecting to the database

---
+++ frauds/src/main/java/com/microservices/frauds/FraudsApplication.java
@@ -0,0 +1,11 @@
+package com.microservices.frauds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FraudsApplication.class, args);
+    }
+}
