81bb50bacbaeccf9fa715132d838604b9aefb5b0
SahnounHoussem <sahnoun.houssem05@gmail.com>
2024-04-02 18:04:31 +0100

    connecting to the database -- create Customer route handler

--- customers/src/main/java/com/microservices/customers/CustomerService.java
+++ customers/src/main/java/com/microservices/customers/CustomerService.java
@@ -3,12 +3,16 @@ package com.microservices.customers;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService (){
+public record CustomerService (
+        CustomersRepository customersRepository
+){
     public void createCustomer(CustomerDto customerDto) {
         Customer customer = Customer.builder()
                 .firstName(customerDto.firstName())
                 .lastName(customerDto.lastName())
                 .email(customerDto.email())
                 .build();
+
+        customersRepository.save(customer);
     }
 }
---
+++ customers/src/main/java/com/microservices/customers/CustomersRepository.java
@@ -0,0 +1,6 @@
+package com.microservices.customers;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomersRepository extends JpaRepository<Customer, Integer> {
+}
