2d62cb9efe849548f016dbae65be82b2a521325e
SahnounHoussem <sahnoun.houssem05@gmail.com>
2024-04-04 17:26:15 +0100

    register eureka clients

--- frauds/src/main/java/com/microservices/frauds/FraudsApplication.java
+++ frauds/src/main/java/com/microservices/frauds/FraudsApplication.java
@@ -2,8 +2,10 @@ package com.microservices.frauds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FraudsApplication {
     public static void main(String[] args) {
         SpringApplication.run(FraudsApplication.class, args);
